dd30535b3528e7b96d9e216d60ace4427c510210
Matt Sandgren <msandgren122@gmail.com>
2019-01-30 14:00:23 -0500

    Created scrape.R, where the functions get pieced together.
    Modified functions.R to actually be only functions.

--- functions.R
+++ functions.R
@@ -1,63 +1,250 @@
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 # functions.R - Matt Sandgren and Zach Snoek
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 
+
+# ********* All code outside of functions should be commented out before **************
+# ********* saving or sourcing. (Except for the libraries())             **************
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# Helpful links
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 # Port in use error?
 # https://stackoverflow.com/questions/43991498/rselenium-server-signals-port-is-already-in-use
 
 # RSelenium docs
 # https://cran.r-project.org/web/packages/RSelenium/RSelenium.pdf
 
-install.packages("tidyverse")
-install.packages("RSelenium")
+
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# Libraries
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# install.packages("tidyverse")
+# install.packages("RSelenium")
 library(tidyverse)
 library(RSelenium)
 
-# Start a Selenium server and client; set Chrome as the client port
-rD <- rsDriver(verbose = FALSE, port = 4444L, browser = "chrome")
-chrome <- rD$client
 
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 # Get Wikipedia's list of hip hop musicians
-chrome$navigate("https://en.wikipedia.org/wiki/List_of_hip_hop_musicians")
-webElems <- chrome$findElements(using = "xpath", value = "//*[@id='mw-content-text']/div/div/ul/li")
-artist_names <- unlist(lapply(webElems, function(x){x$getElementText()}))
-
-# Format artist names for URLs
-for (i in 1:length(artist_names)) {
-  # Remove escaped double quotes
-  artist_names[i] <- gsub("\"", "", artist_names[i])
+# Input: browser session
+# Output: Character vector of cleaned artist names
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+get_artist_names <- function(browser) {
   
-  # Remove Wiki reference numbers
-  artist_names[i] <- gsub("\\[\\d\\]", "", artist_names[i])
+  #navigate to wikipedia page, and get the dirty artist names
+  browser$navigate("https://en.wikipedia.org/wiki/List_of_hip_hop_musicians")
+  message("Going to Wikipedia page...")
+  # pause for 10 seconds, to make sure page is actually loaded
+  Sys.sleep(10)
   
-  artist_names[i] <- gsub("\\.", "", artist_names[i])
-  artist_names[i] <- gsub("\'", "", artist_names[i])
+  message("Finding artist names...")
+  webElems <- browser$findElements(
+    using = "xpath", 
+    value = "//*[@id='mw-content-text']/div/div/ul/li"
+  )
+  artist_names <- unlist(lapply(webElems, function(x){x$getElementText()}))
+  
+  # Format artist names for URLs
+  message("Formatting artist names...")
+  for (i in 1:length(artist_names)) {
+    
+    artist_names[i] <- 
+      artist_names[i] %>% 
+      # Remove escaped double quotes
+      gsub("\"", "", .) %>% 
+      # Remove Wiki reference numbers
+      gsub("\\[\\d\\]", "", .) %>% 
+      gsub("\\.", "", .) %>% 
+      gsub("\'", "", .)
+    
+  }
+  
+  # Remove artists with parentheticals in heading
+  paren <- grep("\\(|\\)", artist_names)
+  artist_names <- artist_names[-paren]
+  
+  # Remove last 3 entries (Wiki headings)
+  artist_names <- artist_names[1:(length(artist_names)-3)]
+  
+  message("Returned artist names")
+  return(artist_names)
 }
 
-# Remove artists with parentheticals in heading
-paren <- grep("\\(|\\)", artist_names)
-artist_names = artist_names[-paren]
+# try it
+# foo <- get_artist_names(chrome)
 
-# Remove last 3 entries (Wiki headings)
-artist_names <- artist_names[1:(length(artist_names)-3)]
 
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 # Construct genius.com artist page urls
-artist_page <- "https://genius.com/artists/" # Every Genius artist page is found in /artists
-
-urls = c()
-
-for (i in 1:length(artist_names)) {
-  # Probably not the right way to do this in R
-  name <- gsub(" ", "-", artist_names[i])
-  name <- paste(artist_page, name, sep = "")
-  urls <- c(urls, name)
+# Input: Browser session
+# Output: Character vector of genius artis page urls
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+make_artist_urls <- function(browser) {
+  
+  message("Getting artist names...")
+  artist_names <- get_artist_names(browser)
+  
+  artist_page <- "https://genius.com/artists/" # Every Genius artist page is found in /artists
+  
+  # empty vector for urls
+  urls <- vector("character", length = 0L)
+  
+  message("Constructing Artist page URLs...")
+  for (i in 1:length(artist_names)) {
+    
+    name <- NULL
+    
+    name <-
+      gsub(" ", "-", artist_names[i]) %>% 
+      paste(artist_page, ., sep = "")
+    
+    urls[i] <- name
+  }
+  
+  message("Returned URLs")
+  return(urls)
+  
 }
 
-urls
+#try it
+# make_artist_urls(chrome)
 
-# Try/catch for URLs
-failed = c()
 
-# Always run these to close out the browser and server.
-# Forgetting to stop the server will give you a 'port already in use' 
-# error the next time you try to start a browser
-chrome$close()
-rD$server$stop()
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# Get album names and urls for an artist
+# Input: browser session, and an artist page
+# Output: named list, containing urls, album names, and years
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+get_albums <- function(browser, artist_page) {
+  
+  #go to an artist page
+  message("Going to Artist page...")
+  browser$navigate(artist_page)
+  Sys.sleep(10)
+  
+  
+  #find the "show all albums button" and click it
+  message("Finding album URLS and titles...")
+  webElem <- chrome$findElements(
+    using = "xpath", 
+    value = "/html/body/routable-page/ng-outlet/routable-profile-page/ng-outlet/routed-page/profile-page/div[3]/div[2]/artist-songs-and-albums/album-grid/div[2]"
+  )
+  
+  unlist(lapply(webElem, function(x){x$getElementText()}))
+  webElem[[1]]$clickElement()
+  
+  
+  #get album URLs
+  webElems <- chrome$findElements(
+    using = "class",
+    value = "mini_card--small"
+  )
+  
+  album_urls <- unlist(lapply(webElems, function(x){x$getElementAttribute("href")}))
+  album_names <- unlist(lapply(webElems, function(x){x$getElementText()})) %>% 
+    strsplit(., split = "\\n")
+  
+  message("Returned discography")
+  return(list(urls = album_urls, info = album_names))
+  
+}
+
+
+#---------------------------------------------------------------------------------------
+# how to use the output from this thing:
+#---------------------------------------------------------------------------------------
+# foo <- get_albums(chrome, "https://genius.com/artists/Kendrick-lamar")
+
+#to get urls:
+# foo$urls
+
+#to get album name and year:
+# foo$info[[1]]
+# foo$info[[5]]
+
+
+#to get album names:
+# foo$info[[1]][1]
+# foo$info[[5]][1]
+
+#to get album years:
+# foo$info[[1]][2]
+# foo$info[[5]][2]
+
+
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# get song URLs from an album page
+# Input: browser session, album url
+# Output: Named list of song URLs and song titles
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+get_songs <- function(browser, album_page) {
+  
+  message("Going to album page...")
+  browser$navigate(album_page)
+  Sys.sleep(10)
+  
+  # get song URLs
+  message("Getting song URLs...")
+  webElems <- browser$findElements(
+    using = "class",
+    value = "u-display_block"
+  )
+  song_urls <- unlist(lapply(webElems, function(x){x$getElementAttribute("href")}))
+  
+  
+  # get song titles
+  message("Getting song titles...")
+  webElems <- browser$findElements(
+    using = "class",
+    value = "chart_row-content-title"
+  )
+  song_names <- unlist(lapply(webElems, function(x){x$getElementText()}))
+  
+  
+  #remove any URL's and title that contain booklet
+  song_urls <- song_urls[!grepl("booklet", song_urls, ignore.case = TRUE)]
+  song_names <- song_names[!grepl("booklet", song_names, ignore.case = TRUE)] %>% 
+    gsub("Lyrics", "", .) %>% 
+    gsub("\\s+$", "", .)
+  
+  message("Returned song information")
+  return(list(urls = song_urls, titles = song_names))
+}
+
+
+# try it
+# foo <- get_songs(chrome, "https://genius.com/albums/Kendrick-lamar/Damn")
+# foo
+# foo$urls
+# foo$titles
