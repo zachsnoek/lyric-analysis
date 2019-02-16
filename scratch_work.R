5daf2c93822388b812eb186f3040149c387e5b5c
zachsnoek <zacharysnoek@hope.edu>
2019-02-16 00:39:09 -0500

    Strip brackets and parentheticals

--- scratch_work.R
+++ scratch_work.R
@@ -4,15 +4,15 @@ install.packages("data.table")
 library(data.table)
 library(dplyr)
 
-#setwd("/Users/zacharysnoek/Programming/r")
-fwrite(lyricsDB, "test_set.csv", row.names = FALSE)
+setwd("/Users/zacharysnoek/Programming/r")
 
-# set your working directory before reading
+#fwrite(lyricsDB, "test_set.csv", row.names = FALSE)
 foo <- fread("test_set.csv", sep2 = "|")
 foo[, collaborators := NULL]
 
 foo <- foo %>% 
-  mutate(lyrics = gsub("\\[.*\\]", "", lyrics)) %>%
-  mutate(lyrics = gsub("\\(.*\\)", "", lyrics))
+  mutate(lyrics = gsub("\\[.*?\\]", "", lyrics)) %>%
+  mutate(lyrics = gsub("\\(.*?\\)", "", lyrics)) %>%
+  mutate(lyrics = gsub("\\\\", "", lyrics))
 
-foo
+foo %>% pull(lyrics)
