740b393e7cd3f0202864c4e54cb24105225b3ccb
Matt Sandgren <msandgren122@gmail.com>
2019-02-16 13:11:13 -0500

    -Better error handling added to scrape.R
    -functions.R renamed to scrape_functions.R
    
    -A few changes to scrape_functions to handle long collaborator lists, and to get artist name/song title from each song page.

--- scrape.R
+++ scrape.R
@@ -21,51 +21,72 @@ chrome <- rD$client
 
 #---------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------
-# piece together functions here
+# create the list of artists and URLs to use in the scrape
 #---------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------
 artist_list <- get_artist_names(chrome)
 artist_urls <- make_artist_urls(chrome)
 
 
+
+chosen <- c("Tupac Shakur", 
+            "The Notorious BIG", 
+            "Dr Dre",
+            "Eminem", 
+            "Kendrick Lamar",
+            "Black Thought", 
+            "Lil Wayne",
+            "Nicki Minaj",
+            "Andy Samberg",
+            "J Cole") %>% 
+  match(., artist_list)
+
+small_artist_list <- artist_list[chosen]
+small_artist_urls <-artist_urls[chosen]
+
 # *** Run these two chunks before the loop. The loop appends to these,
 # *** if they're not empty you'll get duplicates.
 # empty tibble to add to
-lyricsDB <-
-  tibble(
-    artist = character(0),
-    album = character(0),
-    year = character(0),
-    song = character(0),
-    collaborators = character(0),
-    lyrics = character(0),
-    artist_url = character(0),
-    album_url = character(0),
-    song_url = character(0)
-  )
+# lyricsDB <-
+#   tibble(
+#     artist = character(0),
+#     album = character(0),
+#     year = character(0),
+#     song = character(0),
+#     collaborators = character(0),
+#     lyrics = character(0),
+#     artist_url = character(0),
+#     album_url = character(0),
+#     song_url = character(0)
+#   )
 
 # tibble to add failed items to
-failures <-
-  tibble(
-    artist = character(0),
-    album = character(0),
-    year = character(0),
-    song = character(0),
-    collaborators = character(0),
-    lyrics = character(0),
-    artist_url = character(0),
-    album_url = character(0),
-    song_url = character(0)
-  )
+# failures <-
+#   tibble(
+#     artist = character(0),
+#     album = character(0),
+#     year = character(0),
+#     song = character(0),
+#     collaborators = character(0),
+#     lyrics = character(0),
+#     artist_url = character(0),
+#     album_url = character(0),
+#     song_url = character(0)
+#   )
 
 
-#use this one to test
-for(i in 1:5) {
-#use this one for the whole thing  
-# for(i in 1:length(artist_urls)) {
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+# The scrape
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+for(i in 1:length(small_artist_urls)) {
   
-  artist <- artist_list[i]
-  artist_url <- artist_urls[i]
+  #start by getting an artist URL, and their albums
+  artist_url <- small_artist_urls[i]
   albums <- get_albums(chrome, artist_url)
   
   # Make sure there's actually albums for an artist.
@@ -74,15 +95,19 @@ for(i in 1:5) {
   if(identical(FALSE, albums)) {
     failures <- add_row(
       failures,
-      artist = artist,
       artist_url = artist_url
     )
     #move to next artist
     next()
   }
   
+  
+  
+  # ---------------------------------------
   # for each album for this artist
+  # ---------------------------------------
   for(j in 1:length(albums$urls)) {
+    
     album <- albums$info[[j]][1]
     year <- albums$info[[j]][2]
     album_url <- albums$urls[j]
@@ -95,7 +120,6 @@ for(i in 1:5) {
       # uh oh, album not found
       failures <- add_row(
         failures,
-        artist = artist,
         album = album,
         year = year,
         artist_url = artist_url,
@@ -105,11 +129,24 @@ for(i in 1:5) {
       next()
     }
     
+    
+    # ---------------------------------------
     # for each song on this album
+    # ---------------------------------------
     for(k in 1:length(songs$urls)) {
       
       song_url <- songs$urls[k]
-      song_info <- get_song_info(chrome, song_url)
+      
+      # If get_song_info fails, try again
+      song_info <- NULL
+      attempt <- 1
+      while(is.null(song_info) && attempt <= 3) {
+        attempt <- attempt + 1
+        try(
+          song_info <- get_song_info(chrome, song_url)
+        )
+      }
+      
       
       # check if lyrics were found. If not, move to next song
       # and add song to failures tibble
@@ -117,7 +154,6 @@ for(i in 1:5) {
         # uh oh, song not found
         failures <- add_row(
           failures,
-          artist = artist,
           album = album,
           year = year,
           artist_url = artist_url,
@@ -131,6 +167,7 @@ for(i in 1:5) {
         song <- song_info$title
         lyrics <- song_info$lyrics
         collaborators <- song_info$collaborators
+        artist <- song_info$actual_artist
         
         # success! add everything to tibble
         lyricsDB <- add_row(
