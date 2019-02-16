df5a6d5ddda51d829f68e878da1a1dd6606a8e8d
zachsnoek <zacharysnoek@hope.edu>
2019-02-16 01:20:16 -0500

    Add some tidytext analysis

--- scratch_work.R
+++ scratch_work.R
@@ -1,7 +1,9 @@
 # a bit of scratch work
 
 install.packages("data.table")
+install.packages("tidytext")
 library(data.table)
+library(tidytext)
 library(dplyr)
 
 setwd("/Users/zacharysnoek/Programming/r")
@@ -10,9 +12,32 @@ setwd("/Users/zacharysnoek/Programming/r")
 foo <- fread("test_set.csv", sep2 = "|")
 foo[, collaborators := NULL]
 
+# String cleaning
 foo <- foo %>% 
   mutate(lyrics = gsub("\\[.*?\\]", "", lyrics)) %>%
   mutate(lyrics = gsub("\\(.*?\\)", "", lyrics)) %>%
   mutate(lyrics = gsub("\\\\", "", lyrics))
 
-foo %>% pull(lyrics)
+#==================
+# tidytext fun
+#==================
+text <- foo %>% 
+  pull(lyrics)
+lyrics_df <- as_tibble(text)
+
+# Break text into individual tokens
+lyrics_df <- lyrics_df %>%
+  unnest_tokens(word, value)
+
+lyrics_df
+
+# Remove stop words
+data("stop_words")
+lyrics_df <- lyrics_df %>%
+  anti_join(stop_words)
+
+# Simple word count
+lyrics_df <- lyrics_df %>%
+  count(word, sort = TRUE)
+
+lyrics_df
